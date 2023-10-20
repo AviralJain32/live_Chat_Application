fa7f0fee6ff900a93d529c6e2917a1441079c5c3
Aviral jain <aviral2853@gmail.com>
2023-10-20 18:54:41 +0530

    LIVE CHAT APP FIRST COMMIT

---
+++ server/index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+const path = require("path");
+app.use(cors());
+
+const server = http.createServer(app);
+//-------------deployment---------
+const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "../client/dist")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "../client", "dist", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+//-------------deployment----------
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
+const PORT=process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log("SERVER RUNNING");
+});
